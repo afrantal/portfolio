ec79238e0f767dd66dfdbea16b6051da6993e6ef
afrantal <frantalattila@hotmail.com>
2021-01-11 22:59:16 +0100

    second

---
+++ IKT_projektmunka_I/Epulet/epulet.py
@@ -0,0 +1,16 @@
+
+# Épület szigetelés projekt v0.2
+
+szelesseg=0
+hosszusag=0
+magasag=0
+szigeteles=0
+szelesseg=int(input("Kérem a szélességet: "))
+hosszusag=int(input("Kérem a hosszúságot: "))
+magassag=int(input("Kérem a magasságot: "))
+szigeteles=(szelesseg+hosszusag)*magassag*10
+print("Szigetelés=", szigeteles)
+
+#program vége
+
+
---
+++ IKT_projektmunka_I/Park/park.py
@@ -0,0 +1,10 @@
+
+import math
+
+A= 75 
+B= 25 
+C=(A*B*2.5)
+
+print("Gyeptéglák száma" , (math.ceil (C)) , "db") # 0.5 db marad feleslegbe
+
+
